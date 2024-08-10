a9d6bb3207c3f3bc7414a568020a054fc2ea8f3a
Snowworks <snowworksdev@gmail.com>
2024-08-10 15:33:35 +0300

    Checking Errors

--- components/ListingCard.tsx
+++ components/ListingCard.tsx
@@ -84,24 +84,24 @@ const ListingCard: React.FC<ListingCardProps> = ({
 
 export default ListingCard;
 
-export const ListingSkeleton = () => {
-  return (
-    <div className="col-span-1 ">
-      <div className="flex flex-col gap-1 w-full">
-        <Skeleton
-          width={"100%"}
-          height={"100%"}
-          borderRadius={"12px"}
-          className="aspect-square"
-        />
+// export const ListingSkeleton = () => {
+//   return (
+//     <div className="col-span-1 ">
+//       <div className="flex flex-col gap-1 w-full">
+//         <Skeleton
+//           width={"100%"}
+//           height={"100%"}
+//           borderRadius={"12px"}
+//           className="aspect-square"
+//         />
 
-        <div className="flex flex-row gap-3">
-          <Skeleton height={"18px"} width={"84px"} />
-          <Skeleton height={"18px"} width={"84px"} />
-        </div>
-        <Skeleton height={"16px"} width={"102px"} />
-        <Skeleton height={"18px"} width={"132px"} />
-      </div>
-    </div>
-  );
-};
+//         <div className="flex flex-row gap-3">
+//           <Skeleton height={"18px"} width={"84px"} />
+//           <Skeleton height={"18px"} width={"84px"} />
+//         </div>
+//         <Skeleton height={"16px"} width={"102px"} />
+//         <Skeleton height={"18px"} width={"132px"} />
+//       </div>
+//     </div>
+//   );
+// };
